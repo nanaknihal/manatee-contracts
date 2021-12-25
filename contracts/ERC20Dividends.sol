320fa9187ec00d0317aa1ae1e22e14e5783d7c70
nanaknihal <nanaknihal@gmail.com>
2021-12-24 22:33:09 -0800

    added tests cases and their respective fixes

--- contracts/ERC20Dividends.sol
+++ contracts/ERC20Dividends.sol
@@ -13,14 +13,16 @@ contract ERC20Dividends is ERC20 {
   event ERC20PaymentReleased(IERC20 indexed token, address to, uint256 amount);
   event PaymentReceived(address from, uint256 amount);
 
-  IERC20 paymentToken;
+  IERC20 public paymentToken;
   uint256 private _totalReleased;
   mapping(address => uint256) private _released;
   mapping(address => uint256) private _withholding;
 
-  constructor(string memory name, string memory symbol, IERC20 paymentToken) ERC20(name, symbol) payable {
-
+  constructor(string memory name, string memory symbol, IERC20 paymentToken_) ERC20(name, symbol) public payable {
+    paymentToken = paymentToken_;
+    _mint(msg.sender, 1000000000 * 10 ** decimals());
   }
+
 
   function totalReleased() public view returns (uint256) {
       return _totalReleased;
@@ -31,21 +33,31 @@ contract ERC20Dividends is ERC20 {
   }
 
   function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+    //this is not the most gas-efficient, as it is running this if statement for every transfer when it only needs it upon initializating
+    //but it's the easiest way to allow _mint() to be called when totalSupply is 0. totalSupply can only be 0 during initialization (as long as it's initialized with some supply) as this contract does not have public burning functions
+    //it's a low overhead though
+    if(totalSupply() == 0){
+      return;
+    }
     uint256 totalReceived = paymentToken.balanceOf(address(this)) + totalReleased();
     uint256 dividendValue = totalReceived * amount / totalSupply();
-    release(from, dividendValue);
+    _release(from, dividendValue);
     _withholding[to] += dividendValue;
   }
 
-  function release(address account, uint256 amount) public {
-      require(balanceOf(account) > 0, "ERC20Dividends: account has no shares");
-      require(amount != 0, "ERC20Dividends: account is not due payment");
+  function release(uint256 amount) public {
+      _release(msg.sender, amount);
+  }
 
-      _released[account] += amount;
-      _totalReleased += amount;
+  function _release(address account, uint256 amount) private {
+    require(balanceOf(account) > 0, "ERC20Dividends: account has no shares");
+    require(amount <= pendingPayment(account), "ERC20Dividends: amount requested exceeds amount owed");
 
-      SafeERC20.safeTransfer(paymentToken, account, amount);
-      emit ERC20PaymentReleased(paymentToken, account, amount);
+    _released[account] += amount;
+    _totalReleased += amount;
+
+    SafeERC20.safeTransfer(paymentToken, account, amount);
+    emit ERC20PaymentReleased(paymentToken, account, amount);
   }
 
   function pendingPayment(address account) public view returns (uint256) {
--- test/ERC20Dividends.js
+++ test/ERC20Dividends.js
@@ -3,23 +3,100 @@ const { ethers } = require('hardhat');
 
 const pu = ethers.utils.parseUnits
 
-const generateTokenContract = async (fromAddr = null) => {
+//mints full supply to owner
+const generateGenericTokenContract = async (fromAddr = null) => {
+  const GenericTestERC20 = await ethers.getContractFactory('GenericTestERC20');
+  const genericTokenFrom = fromAddr ? GenericTestERC20.connect(fromAddr) : GenericTestERC20;
+  const genericToken = await genericTokenFrom.deploy();
+  await genericToken.deployed();
+  return genericToken;
+}
+
+const generateERC20DividendsTokenContract = async (fromAddr = null) => {
   const ERC20Dividends = await ethers.getContractFactory('ERC20Dividends');
   const e2dFrom = fromAddr ? ERC20Dividends.connect(fromAddr) : ERC20Dividends;
-  const e2d = await e2dFrom.deploy();
+  const e2d = await e2dFrom.deploy('Token Name', 'SYMBOL', (await generateGenericTokenContract()).address);
   await e2d.deployed();
   return e2d;
 }
 
 describe('Regular ERC20 Tests work', function () {
   it('IMPORTANT TODO: RUN THE STANDARD OPENZEPPELIN TESTS ', async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const e = await generateTokenContract();
+    expect(true);
+  });
+});
 
-    expect(await book.name()).to.equal('Name of A Book');
-    expect(await book.price()).to.equal(15000000);
-    expect(await book.totalSupply()).to.equal(1000000000000);
-    expect(await book.balanceOf(owner.address)).to.equal(1000000000000);
 
+describe('Initialization', function() {
+  before(async function() {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    e = await generateERC20DividendsTokenContract();
+  });
+  it('Name and symbol are correctly initialized', async function(){
+    console.log('NAME, SYMBOL', await e.name(), await e.symbol());
+    expect(await e.name()).to.equal('Token Name');
+    expect(await e.symbol()).to.equal('SYMBOL');
+  });
+});
+
+describe('Pending payment calculation', function() {
+  beforeEach(async function() {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    e = await generateERC20DividendsTokenContract();
+    let cf = await ethers.getContractFactory('GenericTestERC20');
+    genericToken = cf.attach(e.paymentToken());
+    //transfer 25% of shares from the owner (who deployed paymentToken and recieved all its supply) to
+    ownerBalance = await e.balanceOf(owner.address);
+    console.log(ownerBalance)
+    // tokenContract.transfer(addr1.address, ownerBalance.mul(3).div(4));
+    genericToken.transfer(e.address, 100000);
+  });
+
+  it('Implement this', async function(){
+
+  });
+
+});
+
+
+describe('Pulling Dividends', function() {
+  beforeEach(async function() {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    e = await generateERC20DividendsTokenContract();
+    let cf = await ethers.getContractFactory('GenericTestERC20');
+    genericToken = cf.attach(e.paymentToken());
+    //transfer 25% of shares from the owner (who deployed paymentToken and recieved all its supply) to
+    // ownerBalance = await e.balanceOf(owner.address);
+    // tokenContract.transfer(addr1.address, ownerBalance.mul(3).div(4));
+    await genericToken.transfer(e.address, 100000);
+
+  });
+  it('Can only pull dividends if has shares', async function(){
+    await expect(e.connect(addr2).release(5)).to.be.revertedWith('ERC20Dividends: account has no shares');
+    await e.connect(owner).release(5);
+  });
+
+  for (const testAmount of [5, 1, 0]) {
+    it('Pulling dividends results in the sender being paid the requested amount balance', async function(){
+      ownerTokenBalance = await genericToken.balanceOf(owner.address);
+      let pending = await e.pendingPayment(owner.address);
+      await e.connect(owner).release(testAmount);
+      expect(await genericToken.balanceOf(owner.address)).to.equal(ownerTokenBalance.add(testAmount));
+      // expect(await e.pendingPayment(owner.address)).to.equal(pending.sub(5));
+    });
+  }
+
+
+
+  // it('Pulling dividends results in the sender being paid its owed balance', async function(){
+  //   pending = await e.pendingPayment(owner.address);
+  //   await e.connect(owner).release(pending.sub(1));
+  //   expect(await e.pendingPayment(owner.address)).to.equal(1);
+  // });
+
+  it('Can only pull the owed amount of dividends (before transfering)', async function(){
+    let pending = await e.pendingPayment(owner.address);
+    await expect(e.release(pending.add(1))).to.be.revertedWith('ERC20Dividends: amount requested exceeds amount owed');
+    await e.release(pending);
   });
 });
